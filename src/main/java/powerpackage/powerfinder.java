b56c63824f5d07091101b2e792a2c981dff33727
adrija-c <146987368+adrija-c@users.noreply.github.com>
2023-10-07 00:17:03 +0530

    Update powerfinder.java

--- src/main/java/powerpackage/powerfinder.java
+++ src/main/java/powerpackage/powerfinder.java
@@ -1,11 +1,8 @@
 package powerpackage;
 
-public class powerfinder {
-    public static int OF(int xyz, int pqr) {
-        int p = 1;
-        for (int i = 0; i<pqr; i++) {
-            p *= xyz;
-        }
-        return p;
+public class PowerFinder {
+
+    public int calculatePower(int x, int y) {
+        return (int) Math.pow(x, y);
     }
 }
